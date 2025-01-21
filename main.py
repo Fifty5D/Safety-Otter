6d01bd36b5b28c58d26f3ff1ef1d13e55127b51b
Fifty5D <110144347+Fifty5D@users.noreply.github.com>
2025-01-20 23:27:52 -0600

    fix(main): Slash commands fully functional

--- main.py
+++ main.py
@@ -22,18 +22,9 @@ client = SafetyOtter(intents=discord.Intents.all())
 @client.event
 async def on_ready():
     try:
-        for _x in client.guilds:
-            print(f"Clearing {_x.name}")
-            GUILD=discord.Object(_x.id)
-            client.tree.clear_commands(guild=GUILD)
-            client.tree.copy_global_to(guild=GUILD)
-        print("Cleared all commands")
-        client.tree.add_command(maintenance, guild=GUILD)
-        print("Added maintenance command")
-        client.tree.add_command(add_admin, guild=GUILD)
-        print("Added add admin command")
+        GUILD = discord.Object(secret.GUILD_ID)
         print("Syncing commands")
-        await client.tree.sync()
+        await client.tree.sync(guild=GUILD)
         print("Synced commands")
     except discord.HTTPException:
         print("Failed to sync commands. This is likely due to a missing permission.")
@@ -72,57 +63,19 @@ async def configure_list(ctx: commands.Context):
     else:
         await ctx.send("You do not have permission to use this command.")
 
-# #Ping pong command
-# @client.command()
-# async def ping(ctx: commands.Context):
-#     await ctx.send(f"Pong!")
-
-# #Maintenance Request Command
-# @client.command(name="request", help="Send a maintenance request to the maintenance team.")
-# async def request(ctx: commands.Context, *, message: str):
-#     channel = ctx.channel
-#     role = ctx.guild.get_role(config[str(ctx.guild.id)]["roles"]["maintenance_team"])
-#     await channel.send(f"{role.mention} Maintenance request from {ctx.author}: {message}")
-#     await ctx.send("Your request has been sent to the maintenance team.")
-
-# #Add user as bot admin
-# @client.command()
-# async def add_bot_admin(ctx: commands.Context, add_admin: int):
-#     if ctx.author.guild_permissions.administrator:
-#         if str(add_admin) in config[str(ctx.guild.id)]["admins"]:
-#             await ctx.send(f"<@{add_admin}> is already an admin.")
-#             return
-#         config[str(ctx.guild.id)]["admins"][str(add_admin)] = add_admin
-#         with open("config.json", "w") as f:
-#             json.dump(config, f, indent=4)
-#         await ctx.send(f"Added <@{add_admin}> as an admin.")
-#     else:
-#         await ctx.send("You do not have permission to use this command.")
-
-# #Remove user as bot admin
-# @client.command()
-# async def remove_admin(ctx: commands.Context, remove_admin: int):
-#     if ctx.author.guild_permissions.administrator:
-#         if str(remove_admin) not in config[str(ctx.guild.id)]["admins"]:
-#             await ctx.send(f"<@{remove_admin}> is not an admin.")
-#         else:
-#             del config[str(ctx.guild.id)]["admins"][str(remove_admin)]
-#             with open("config.json", "w") as f:
-#                 json.dump(config, f, indent=4)
-#             await ctx.send(f"Removed <@{remove_admin}> as an admin.")
-#     else:
-#         await ctx.send("You do not have permission to use this command.")
-
 #Maintenance / Command
-@discord.app_commands.command(name="maintenance", description="Send a maintenance request to the maintenance team.")
+@client.tree.command(name="maintenance", description="Send a maintenance request to the maintenance team.")
 async def maintenance(interaction: discord.Interaction, message: str):
-    channel = interaction.channel
-    role = interaction.guild.get_role(config[str(interaction.guild.id)]["roles"]["maintenance_team"])
-    await channel.send(f"{role.mention} Maintenance request from <@{int(interaction.user.id)}>: {message}")
-    await interaction.response.send_message("Your request has been sent to the maintenance team.")
+    try:
+        channel = interaction.channel
+        role = interaction.guild.get_role(config[str(interaction.guild.id)]["roles"]["maintenance_team"])
+        await channel.send(f"{role.mention} Maintenance request from <@{int(interaction.user.id)}>: {message}")
+        await interaction.response.send_message("Your request has been sent to the maintenance team.")
+    except discord.HTTPException:
+        await interaction.response.send_message("An error occurred while trying to send the maintenance request. Please try again later.")
 
 #Add admin / command
-@discord.app_commands.command(name="add_admin", description="Add a user as an admin.")
+@client.tree.command(name="add_admin", description="Add a user as an admin.")
 async def add_admin(interaction: discord.Interaction, add_admin: discord.User):
     try:
         if interaction.user.guild_permissions.administrator:
